7437f39dd6a5149549d42878125de2491859364c
gamesnakepython <56094751+gamesnakepython@users.noreply.github.com>
2019-10-03 01:34:40 -0300

    alterações para animar logo cobra

--- js/funcao.js
+++ js/funcao.js
@@ -1,7 +1,17 @@
-function imgChange(logo) {
-    document.getElementById('olhosPiscando').src = (logo);
-    setTimeout()
+function pararOlhos(logo){
+    clearTimeout(olhos);
+    document.getElementById('olhosPiscando').src = ('image/logo.png');
 }
+
+function piscarOlhos(logo){
+    setTimeout(function () { document.getElementById('olhosPiscando').src = ('image/logof.png') }, 200);
+    document.getElementById('sonsStart').play();
+    setTimeout(function () { document.getElementById('olhosPiscando').src = ('image/logo.png') }, 500);
+}
+function intervaloPiscarOlhos(logo){
+    setInterval(function () { piscarOlhos(logo) }, 12000);
+}
+
 
 function playAudio(audio) {
     document.getElementById(audio).play();
